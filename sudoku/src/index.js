dcf475a2b39fc0f2aacb4adfe0742248183f86c0
Vignesh Muthukumaran <mvignesh243@gmail.com>
2021-01-19 13:50:48 +0000

    sudoku solver using react

---
+++ sudoku/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from './Board.js';
+import Menu from './Menu.js';
+// import 'bootstrap/dist/css/bootstrap.css';
+var _ = require('lodash');
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initial: 33
+    };
+  }
+//   handleGeneration = (initial) => {
+//     this.setState({initial: initial});
+//   };
+
+  render() {
+      return(<p>ASDF</p>)
+    // return (<div className="game">
+    //   <div className="game-board">
+    //     <Board key={_.random(0, 10000)} initial={this.state.initial}/>
+    //     <div className="game-menu">
+    //       <Menu onGenerate={this.handleGeneration}/>
+    //     </div>
+    //   </div>
+    //   <a className="github-link" href="https://github.com/vigneshm243">View Code on Github.</a>
+    // </div>);
+  }
+}
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
